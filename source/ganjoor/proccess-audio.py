d606e4033ae8802286b31938efb64cba145dc1eb
Hasan Mirtalebi <mirtalebi83@gmail.com>
2025-02-24 15:41:16 +0330

    eX

--- source/ganjoor/proccess-audio.py
+++ source/ganjoor/proccess-audio.py
@@ -24,25 +24,31 @@ for row in data:
     if (downloaded == 1):
         continue
 
-    url = "https://i.ganjoor.net/ak/5000-fz-1532962519.mp3"
     # Create the directory if it doesn't exist
     os.makedirs('../../ganjoor/' + audioArtist, exist_ok=True)
 
     # Download the audio file
-    response = requests.get(url)
+    response = requests.get(mp3Url)
     if response.status_code == 200:
         file_path = os.path.join('../../ganjoor/' + audioArtist, f'{id}.mp3')
         with open(file_path, 'wb') as file:
             file.write(response.content)
-
-        # Update the database to set downloaded to 1
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("UPDATE data SET downloaded = 1 WHERE id = ?", (id,))
-        conn.commit()
-        conn.close()
-        
     else:
-        print(f'Failed to download {url}')
+        print(f'Failed to download {mp3Url}')
+        continue
+
+    # Download the XML file
+    response = requests.get(xmlText)
+    if response.status_code == 200:
+        xml_file_path = os.path.join('../../ganjoor/' + audioArtist, f'{id}.xml')
+        with open(xml_file_path, 'wb') as file:
+            file.write(response.content)
+    else:
+        print(f'Failed to download {xmlText}')
+        continue
     
-    print(f'{id}: {poemFullTitle} - {audioTitle} by {audioArtist}')
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute("UPDATE data SET downloaded = 1 WHERE id = ?", (id,))
+    conn.commit()
+    conn.close()
