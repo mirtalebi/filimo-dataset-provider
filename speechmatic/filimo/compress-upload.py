436a85ca64d2bf148a026aca12913a3a2fbba569
Hasan Mirtalebi <mirtalebi83@gmail.com>
2025-03-25 23:40:20 +0330

    eX

--- speechmatic/filimo/compress-upload.py
+++ speechmatic/filimo/compress-upload.py
@@ -37,7 +37,7 @@ create_tar_gz(output_tar_gz, folder_to_compress)
 
 api.upload_file(
     path_or_fileobj=output_tar_gz,
-    path_in_repo="batch_{batch_number}.tar.gz",
+    path_in_repo=f"batch_{batch_number}.tar.gz",
     repo_id="farsi-asr/filimo-chunked-asr-dataset",
     repo_type="dataset",
 )
