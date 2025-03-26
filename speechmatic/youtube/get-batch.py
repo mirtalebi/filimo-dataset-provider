15238abe568c79efd0145032d103bfa02b6353e4
Hasan Mirtalebi <mirtalebi83@gmail.com>
2025-03-27 00:57:06 +0330

    eX

--- speechmatic/youtube/get-batch.py
+++ speechmatic/youtube/get-batch.py
@@ -3,6 +3,7 @@ from huggingface_hub import hf_hub_download
 
 def download_batch(batch_name):
   filename = batch_name + ".tar.gz"
+  print(filename)
   hf_hub_download(repo_id="farsi-asr/filimo-chunked-asr-dataset", filename=filename, repo_type="dataset", local_dir="content")
   tar = tarfile.open("content/" + filename)
   tar.extractall(path="content/")
@@ -14,6 +15,7 @@ def download_batch(batch_name):
 try:
   with open("batch_name.txt", "r") as file:
     batch_name = file.read().strip()
+    print(batch_name)
     download_batch(batch_name)
 
 except FileNotFoundError:
