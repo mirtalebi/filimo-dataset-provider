2664566f17e4e76d9f0f02852d729b170d01e78b
Hasan Mirtalebi <mirtalebi83@gmail.com>
2025-05-24 14:06:14 +0330

    EX

--- speechmatic/tarjoman/main.py
+++ speechmatic/tarjoman/main.py
@@ -147,7 +147,7 @@ def proccess_batch(name, semaphore):
 
 
 def proccess():
-    max_threads = 2
+    max_threads = 1
     semaphore = threading.Semaphore(max_threads)
     threads = []
     fileList = fs.ls("datasets/farsi-asr/PerSets-tarjoman-chunked", detail=True)
